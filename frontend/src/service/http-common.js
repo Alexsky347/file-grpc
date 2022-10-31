631a55845c820199bdadcabb87ceafae70702477
ald <alexis.ledain@itesoft.com>
2022-10-31 10:23:52 +0100

    env front not working

--- frontend/src/service/http-common.js
+++ frontend/src/service/http-common.js
@@ -2,5 +2,5 @@ import axios from "axios";
 require('dotenv').config();
 
 export default axios.create({
-  baseURL: process.env.BACKEND_URL || 'http://localhost:8080/api'
+  baseURL: process.env.BACKEND_URL || 'http://ledanalexis.com:8080/api'
 });
