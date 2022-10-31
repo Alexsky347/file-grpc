dfe0f92e4a7044d5b9822652f090cdbfb2e76bab
ald <alexis.ledain@itesoft.com>
2022-10-31 10:30:26 +0100

    add dotenv in index.js

--- frontend/src/service/http-common.js
+++ frontend/src/service/http-common.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.BACKEND_URL || 'http://localhost:8080/api'
+  baseURL: process.env.BACKEND_URL || 'http://ledainalexis.com:8080/api'
 });
