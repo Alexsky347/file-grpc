f20cc903be5b079b2df10031fdf92ff6e5245803
alexis le dain <ledain.alexis@gmail.com>
2022-11-11 10:55:29 +0100

    fix docker compose

--- frontend/src/service/http-common.js
+++ frontend/src/service/http-common.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env?.REACT_APP_BACKEND_URL ?? 'http://localhost:3001',
+  baseURL: process.env?.REACT_APP_BACKEND_URL ?? 'http://localhost:3000',
 });
