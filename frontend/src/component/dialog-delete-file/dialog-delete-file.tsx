b87390756fbf65048884745b23a0b332e35b0578
Alexsky347 <ledain.alexis@gmail.com>
2024-01-25 17:30:53 +0100

    add suspense on delete file

--- frontend/src/component/dialog-delete-file/dialog-delete-file.tsx
+++ frontend/src/component/dialog-delete-file/dialog-delete-file.tsx
@@ -17,40 +17,49 @@ interface DialogDeleteFileProps {
 
 export default function DialogDeleteFile({metaData, reRender, setReRender}: Readonly<DialogDeleteFileProps>) {
     const dispatch = useDispatch<AppDispatch>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const handleDelete = async () => {
+        setIsLoading(prevState => !prevState);
         const response: any = await dispatch(deleteFile(metaData));
         if (!response?.error) {
             setReRender(prev => !prev);
         }
+        setIsLoading(prevState => !prevState);
     }
     return (
-        <AlertDialog.Root>
-            <AlertDialog.Trigger>
-                <button
-                    className="btn btn-error">
-                    <TrashIcon/>
-                </button>
-            </AlertDialog.Trigger>
-            <AlertDialog.Content style={{maxWidth: 450}}>
-                <AlertDialog.Title>Delete Item</AlertDialog.Title>
-                <AlertDialog.Description size="2">
-                    Are you sure? This picture will no longer be accessible.
-                </AlertDialog.Description>
+        <>
+        {isLoading ? (
+                <div className="flex items-center justify-center h-screen">
+                    <span className="loading loading-dots loading-lg"></span>
+                </div>
+            ) : <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <button
+                        className="btn btn-error">
+                        <TrashIcon/>
+                    </button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content style={{maxWidth: 450}}>
+                    <AlertDialog.Title>Delete Item</AlertDialog.Title>
+                    <AlertDialog.Description size="2">
+                        Are you sure? This picture will no longer be accessible.
+                    </AlertDialog.Description>
 
-                <Flex gap="3" mt="4" justify="end">
-                    <AlertDialog.Cancel>
-                        <Button variant="soft" color="gray">
-                            Cancel
-                        </Button>
-                    </AlertDialog.Cancel>
-                    <AlertDialog.Action>
-                        <Button variant="solid" color="red" onClick={handleDelete}>
-                            Delete
-                        </Button>
-                    </AlertDialog.Action>
-                </Flex>
-            </AlertDialog.Content>
-        </AlertDialog.Root>
+                    <Flex gap="3" mt="4" justify="end">
+                        <AlertDialog.Cancel>
+                            <Button variant="soft" color="gray">
+                                Cancel
+                            </Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                            <Button variant="solid" color="red" onClick={handleDelete}>
+                                Delete
+                            </Button>
+                        </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>}
+        </>
     );
 }
